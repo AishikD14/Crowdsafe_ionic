b829978847ccce0ef63a5eee6420b3cb2f53cf82
Aishik Deb <debaishik14@gmail.com>
2020-04-22 03:51:35 +0530

    'routing''

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,14 +6,14 @@ const routes: Routes = [
   //   path: '',
   //   loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
   // },
-  { path: '', redirectTo: 'menu', pathMatch: 'full'},
+  //{ path: '', redirectTo: 'menu', pathMatch: 'full'},
   { path: '', loadChildren: './pages/menu/menu.module#MenuPageModule'},
-  { path: '/menu', redirectTo: 'menu', pathMatch: 'full'},
-  { path: '/menu/login', redirectTo: 'menu', pathMatch: 'full'},
-  { path: '/menu/register', redirectTo: 'menu', pathMatch: 'full'},
-  { path: '/menu/heatmap', redirectTo: 'menu', pathMatch: 'full'},
-  { path: '/menu/mainpage', redirectTo: 'menu', pathMatch: 'full'},
-  { path: '/menu/upload', redirectTo: 'menu', pathMatch: 'full'}
+  { path: 'menu', loadChildren: './pages/menu/menu.module#MenuPageModule'},
+  { path: 'menu/login', loadChildren: './pages/menu/menu.module#MenuPageModule'},
+  { path: 'menu/register', loadChildren: './pages/menu/menu.module#MenuPageModule'},
+  { path: 'menu/heatmap', loadChildren: './pages/menu/menu.module#MenuPageModule'},
+  { path: 'menu/mainpage', loadChildren: './pages/menu/menu.module#MenuPageModule'},
+  { path: 'menu/upload', loadChildren: './pages/menu/menu.module#MenuPageModule'}
 
 ];
 @NgModule({
